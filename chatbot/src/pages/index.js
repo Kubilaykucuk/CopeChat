ce09e267c958833dfd92cc22bf3051525611be9b
Kubilaykucuk <kubilaykucuk510@gmail.com>
2023-06-23 12:48:37 +0300

    First Commit

---
+++ chatbot/src/pages/index.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Chatbot from '../pages/chatbot'
+import styles from '@/styles/Home.module.css'
+import axios from 'axios';
+import TypingAnimation from "../components/TypingAnimation";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const [show, setShow] = useState(false);
+
+  const handleClick = (event) => 
+  {
+    setShow(!show);
+  }
+  
+  return (
+    <div className="fixed bottom-0 right-0 flex h-screen items-end justify-end p-5">
+      {show && <div className="">
+        <Chatbot />
+        </div>}
+      <button className="flex items-center m-5 bg-transparent rounded" onClick={handleClick}>
+        <Image src="/copetract_logo1.png" alt="Logo" width={60} height={60} />
+      </button>
+    </div>
+  );
+}
